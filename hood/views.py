ebf8df4f65cfdfb912f8e1b407c5445805144a2b
clara chepngeno metto <clara.metto@student.moringaschool.com>
2022-01-08 15:13:51 +0300

    update views

--- hood/views.py
+++ hood/views.py
@@ -10,15 +10,16 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-# Create your views here.
+# views here.
 def index(request):
     current_user = request.user
 
     profiles = Profile.objects.filter(user_id = current_user.id).all()
     return render(request, 'index.html',{'profiles':profiles,})
     
+
 @login_required(login_url='/accounts/login/')
-def update_profile(request):
+def create_profile(request):
     current_user = request.user
     form = ProfileForm(request.POST, request.FILES)
     if request.method == 'POST':
@@ -29,12 +30,55 @@ def update_profile(request):
             return redirect ('index')
         else:
             form = ProfileForm()
-    return render(request, 'create_profile.html', {'form': form})
-
-@login_required(login_url='/accounts/login/')
-def profile(request,pk):
-    user = User.objects.get(pk = pk)
-    profiles = Profile.objects.filter(user = user).all()
+    return render(request, 'create_profile.html', {'form': form}
+    )
+@login_required(login_url="/accounts/login/")
+def profile(request):
     current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    ctx = {"profile": profile}
+    return render(request, "profile.html", ctx)
 
-    return render(request,'profile.html',{"current_user":current_user, "user":user, "profiles":profiles})
+@login_required(login_url="/accounts/login/")
+def update_profile(request,id):
+    user = User.objects.get(id=id)
+    profile = Profile.objects.get(user = user)
+    form = UpdateProfileForm(instance=profile)
+    if request.method == "POST":
+            form = UpdateProfileForm(request.POST,request.FILES,instance=profile)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.save()
+                return redirect('profile')
+    ctx = {"form":form}
+    return render(request, 'update_profile.html', ctx)
+
+@login_required(login_url="/accounts/login/")
+def create_hood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        hood_form = CreateHoodForm(request.POST, request.FILES)
+        if hood_form.is_valid():
+            hood = hood_form.save(commit=False)
+            hood.user = current_user
+            hood.save()
+        return HttpResponseRedirect('/profile')
+    else:
+        hood_form = CreateHoodForm()
+    context = {'hood_form':hood_form}
+    return render(request, 'hood/create_hood.html',context)
+
+@login_required(login_url="/accounts/login/")
+def hoods(request):
+    current_user = request.user
+    hood = NeighbourHood.objects.all().order_by('-id')
+
+    context ={'hood':hood}
+    return render(request, 'hood/hood.html', context)
+
+
+@login_required(login_url="/accounts/login/")
+def single_hood(request,name):
+    hood = NeighbourHood.objects.get(name=name)
+    ctx = {"hood":hood}
+    return render(request, 'hood/single_hood.html', ctx)
