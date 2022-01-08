855c7aea4e73b29495d8f8fef2e31dd270c2ef6b
clara chepngeno metto <clara.metto@student.moringaschool.com>
2022-01-08 14:19:57 +0300

    config urls in the neighbor

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', views.logout_then_login, name='logout'),
+    
 ]
