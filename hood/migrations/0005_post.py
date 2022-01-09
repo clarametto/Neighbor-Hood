08c66321e9524e81f2b0b6cc9d2c677d900c3e98
clara chepngeno metto <clara.metto@student.moringaschool.com>
2022-01-10 02:37:30 +0300

    update post.py

---
+++ hood/migrations/0005_post.py
@@ -0,0 +1,31 @@
+import cloudinary.models
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('hood', '0004_alter_neighbourhood_admin'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('post_image', cloudinary.models.CloudinaryField(max_length=255, null=True, verbose_name='post_image')),
+                ('post_description', models.TextField(max_length=1000)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('hood', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='hood_post', to='hood.neighbourhood')),
+                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-pk'],
+            },
+        ),
+    ]
