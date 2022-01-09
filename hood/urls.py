be39770afb6ddf7a4041d8514c65743145d8dcf3
clara chepngeno metto <clara.metto@student.moringaschool.com>
2022-01-10 02:13:50 +0300

    modified urls

--- hood/urls.py
+++ hood/urls.py
@@ -9,7 +9,10 @@ urlpatterns=[
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     path('create_hood',views.create_hood, name= 'create_hood'),
     path('hood/', views.hoods, name = 'hood'),
-    # path('hood/<str:name>',views.single_hood,name='single_hood'),
-
+    path('join_hood/<name>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('<str:name>/hood',views.single_hood,name='single_hood'),
+    path('create_post',views.create_post, name= 'create_post'),
+    path('create_business',views.create_business, name= 'create_business'),
 ]
 
