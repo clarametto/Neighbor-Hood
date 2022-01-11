17d92a1b4fad3d525029034530f5f45ddd948ae1
clara chepngeno metto <clara.metto@student.moringaschool.com>
2022-01-11 12:56:18 +0300

    deployment to heroku

--- neighbor/settings.py
+++ neighbor/settings.py
@@ -78,7 +78,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'neighbor.urls'
 
